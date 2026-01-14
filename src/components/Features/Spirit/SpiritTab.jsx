d4acbac19ef9408d030322fb5f124eefb46da74a
no-kris <kris.tresk@gmail.com>
2026-01-13 19:27:32 -0500

    Initialized structure for the spirit tab component.

--- src/components/Features/Spirit/SpiritTab.jsx
+++ src/components/Features/Spirit/SpiritTab.jsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+import ProgressBar from "../../Misc/ProgressBar";
+import SpiritPet from "./SpiritPet";
+import Stats from "../../Misc/Stats";
+import Badges from "../../Misc/Badges";
+
 export default function SpiritTab() {
-  return <div>Spirit Tab</div>;
+  const [profileStats, setProfileStats] = useState({});
+
+  return (
+    <div className="spirit-container">
+      <div className="spirit-container__header">
+        <h3>My Spirit Companion</h3>
+        <p>Form: (spirit pet)</p>
+      </div>
+      <div className="spirit-container__pet">
+        <SpiritPet profileStats={profileStats} />
+      </div>
+      <ProgressBar profileStats={profileStats} />
+      <Stats profileStats={profileStats} />
+      <div>
+        <Badges />
+      </div>
+    </div>
+  );
 }
